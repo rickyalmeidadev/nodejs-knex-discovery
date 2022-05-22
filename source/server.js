cacf3cb236969dfe7ac53edcd109f7e31c54ac43
Ricky Almeida <ricky.almeida.dev@gmail.com>
2022-05-22 05:20:10 -0300

    configure cors

--- source/server.js
+++ source/server.js
@@ -1,12 +1,20 @@
 import "express-async-errors";
 import express from "express";
-import authors from "./modules/authors/routes";
+import cors from "cors";
+import morgan from "morgan";
+import authors from "~/modules/authors/routes";
 import books from "~/modules/books/routes";
-import HttpError from "./shared/http-errors";
+import HttpError from "~/shared/http-errors";
+import environment from "~/environment";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
+
+if (environment.NODE_ENV === "development") {
+  server.use(morgan("dev"));
+}
 
 server.get("/", (_, response) =>
   response.send("Welcome to Node.js Knex Discovery!")
